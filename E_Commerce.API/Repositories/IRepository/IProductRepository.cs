035248fabddf03f2ccb55a9b2147256d635ccdff
MinhIT269 <minhnguyen.260903@gmail.com>
2025-04-15 01:27:19 +0700

    Home UI

--- E_Commerce.API/Repositories/IRepository/IProductRepository.cs
+++ E_Commerce.API/Repositories/IRepository/IProductRepository.cs
@@ -6,9 +6,9 @@ namespace E_Commerce.API.Repositories.IRepository
     {
         Task<List<Product>> GetAllProductsAsync();
         Task<List<Product>> GetAllAsync(string? filterQuery, string sortBy, bool isAscending, int pageNumber, int pageSize);
+        Task<List<Product>> GetProductsByCategoryAsync(Guid categoryId, int count);
         Task<List<Product>> FindProductsByBrandAsync(string filterQuery, Guid brandId);
         Task<List<Product>> FindProductsByNameAsync(string name);
         Task<int> CountProductAsync();
-
     }
 }
---
+++ E_Commerce.UI/Models/Responses/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.UI.Models.Responses
+{
+    public class HomeViewModel
+    {
+        public List<BrandResponseDto>? Brands { get; set; } 
+        public List<CategoryResponseDto>? Categories { get; set; }
+        public List<CategoryProductDto> CategoryBlocks { get; set; } = new List<CategoryProductDto>();
+    }
+}
