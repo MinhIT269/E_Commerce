68dd13835d1ffacc75ae20bb501ad344e1d356d2
MinhIT269 <minhnguyen.260903@gmail.com>
2025-05-17 20:45:39 +0700

    Local chatbox

--- E_Commerce.UI/Areas/User/Views/ViewComponents/ChatbotViewComponent.cs
+++ E_Commerce.UI/Areas/User/Views/ViewComponents/ChatbotViewComponent.cs
@@ -4,8 +4,15 @@ namespace E_Commerce.UI.Areas.User.Views.ViewComponents
 {
     public class ChatbotViewComponent : ViewComponent
     {
+        private readonly IConfiguration _config;
+        public ChatbotViewComponent(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
         public IViewComponentResult Invoke()
         {
+            var apiUrl = _config["Chatbot:ApiUrl"];
+            ViewBag.apiUrl_ChatBot = apiUrl;
             return View();
         }
     }
